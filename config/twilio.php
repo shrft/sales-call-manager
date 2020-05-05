07e0a9ec308eb3ae9ab6b0cd722d7e73c721a168
shirafuta <shirafuta.kodai@gmail.com>
2020-05-05 17:14:15 +0900

    ngrox経由でhttp

---
+++ config/twilio.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'accountSid' => getenv('TWILIO_ACCOUNT_SID'),
+    'authToken' => getenv('TWILIO_AUTH_TOKEN'),
+    'TWILIO_NUMBER' => getenv('TWILIO_NUMBER'),
+    'applicationSid' => getenv('TWILIO_APPLICATION_SID'),
+);
