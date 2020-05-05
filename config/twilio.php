cc0cc9b9f7551def5189f0b75b3f37fcc69c1bdd
shirafuta <shirafuta.kodai@gmail.com>
2020-05-05 18:46:05 +0900

    fix bug

--- config/twilio.php
+++ config/twilio.php
@@ -2,6 +2,6 @@
 return array(
     'accountSid' => getenv('TWILIO_ACCOUNT_SID'),
     'authToken' => getenv('TWILIO_AUTH_TOKEN'),
-    'TWILIO_NUMBER' => getenv('TWILIO_NUMBER'),
+    'number' => getenv('TWILIO_NUMBER'),
     'applicationSid' => getenv('TWILIO_APPLICATION_SID'),
 );
